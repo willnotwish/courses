85219b5b15beb0930f382ac38d395f595f76150a
Nick Adams <nick@nickadams.co.uk>
2018-10-08 18:23:10 +0100

    Missing module line in admin/dashboard_controller

--- app/controllers/courses/admin/dashboard_controller.rb
+++ app/controllers/courses/admin/dashboard_controller.rb
@@ -1,6 +1,7 @@
 require_dependency "courses/application_controller"
 
 module Courses
+  module Admin
 	  class DashboardController < ApplicationController
 	    def show
 	    	@dashboard = Dashboard.new
